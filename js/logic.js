486da23bc88831325fe6a5ada711bd10ad58a7ff
Mustafa Kaya <iletisim.mustafakaya@gmail.com>
2022-02-19 18:43:19 +0300

    Update logic.js

--- js/logic.js
+++ js/logic.js
@@ -47,7 +47,120 @@ function updateScore(roundWinner) {
 
   if (roundWinner === 'Player') {
     document.getElementById('player-score').textContent = playerScore + 1
-  } else if (roundWinner === 'Computer') {
+  }
+  else if (roundWinner === 'Computer') {
     document.getElementById('computer-score').textContent = computerScore + 1
   }
+}
+
+function viewPlayerSelection(event, roundWinner) {
+  if (roundWinner === 'Player') {
+    event.target.classList.add('button--won', 'button--disable')
+  }
+  else if (roundWinner === 'Computer') {
+    event.target.classList.add('button--lost', 'button--disable')
+  }
+  else {
+    event.target.classList.add('button--tie', 'button--disable')
+  }
+}
+
+function hideOtherSelections(event) {
+  let selection = event.target.id;
+  let rock = document.getElementById('rock');
+  let paper = document.getElementById('paper');
+  let scissors = document.getElementById('scissors');
+
+  if (selection === 'rock') {
+    paper.classList.add('button--unselected', 'button--disable');
+    scissors.classList.add('button--unselected', 'button--disable');
+  }
+  else if (selection === 'paper') {
+    scissors.classList.add('button--unselected', 'button--disable');
+    rock.classList.add('button--unselected', 'button--disable');
+  }
+  else if (selection === 'scissors') {
+    rock.classList.add('button--unselected', 'button--disable');
+    paper.classList.add('button--unselected', 'button--disable');
+  }
+}
+
+function viewComputerSelection(computerWeapon) {
+  let computerButton = document.getElementById('computer');
+  if (computerWeapon === 'rock') {
+    computerButton.children[0].src = computerButton.children[0].src.replace(/rock|paper|scissors/, 'rock');
+    computerButton.children[1].textContent = 'ROCK';
+  }
+  else if (computerWeapon === 'paper') {
+    computerButton.children[0].src = computerButton.children[0].src.replace(/rock|paper|scissors/, 'paper');
+    computerButton.children[1].textContent = 'PAPER';
+  }
+  else if (computerWeapon === 'scissors') {
+    computerButton.children[0].src = computerButton.children[0].src.replace(/rock|paper|scissors/, 'scissors');
+    computerButton.children[1].textContent = 'SCISSORS';
+  }
+}
+
+function resetRound() {
+  let computerButton = document.getElementById('computer');
+  let rock = document.getElementById('rock');
+  let paper = document.getElementById('paper');
+  let scissors = document.getElementById('scissors');
+
+  rock.classList.remove('button--unselected', 'button--disable', 'button--won', 'button--tie', 'button--lost');
+  paper.classList.remove('button--unselected', 'button--disable', 'button--won', 'button--tie', 'button--lost');
+  scissors.classList.remove('button--unselected', 'button--disable', 'button--won', 'button--tie', 'button--lost');
+
+  computerButton.children[0].src = computerButton.children[0].src.replace(/rock|paper|scissors/, 'rock');
+  computerButton.children[1].textContent = '***';
+
+  document.getElementsByClassName('info')[0].style.display = 'none'
+  document.getElementsByClassName('info__subtext')[0].textContent = 'New Round in 3'
+}
+
+// kimin kazandigini burada karar verip
+// 
+
+function checkScore() {
+
+  let totalScore = 3;
+
+  let playerScore = Number(document.getElementById('player-score').textContent);
+  let computerScore = Number(document.getElementById('computer-score').textContent);
+
+  if (playerScore === totalScore || computerScore === totalScore) {
+    if (playerScore > computerScore) {
+      document.getElementsByClassName('result__text')[0].style.display = 'block'
+      document.getElementsByClassName('result__text')[0].textContent = 'YOU WON'
+    } else {
+      document.getElementsByClassName('result__text')[0].style.display = 'block'
+      document.getElementsByClassName('result__text')[0].textContent = 'GAME OVER'
+    }
+    document.getElementsByClassName('game')[0].style.display = 'none'
+    document.getElementsByClassName('info')[0].style.display = 'none'
+  }
+}
+
+function viewRoundResult(roundWinner) {
+  document.getElementsByClassName('info')[0].style.display = 'flex'
+
+  if (roundWinner === 'Player') {
+    document.getElementsByClassName('info__text')[0].textContent = 'You Won Round';
+  }
+  else if (roundWinner === 'Computer') {
+    document.getElementsByClassName('info__text')[0].textContent = 'You Lost Round';
+  } else if (roundWinner === 'Tie') {
+    document.getElementsByClassName('info__text')[0].textContent = 'Tie'
+  }
+
+  roundCountdown()
+}
+
+function roundCountdown() {
+  setTimeout(() => {
+    document.getElementsByClassName('info__subtext')[0].textContent = 'New Round in 2'
+  }, 1000)
+  setTimeout(() => {
+    document.getElementsByClassName('info__subtext')[0].textContent = 'New Round in 1'
+  }, 2000)
 }
--- js/script.js
+++ js/script.js
@@ -4,12 +4,20 @@ for (const button of buttons) {
   const buttonIcon = button.children[1];
   button.addEventListener('mouseenter', addAnimation);
   buttonIcon.addEventListener('animationend', removeAnimation);
-  button.addEventListener('click', playRound);
+  button.addEventListener('click', createNewRound);
 }
 
-function playRound(event) {
+function createNewRound(event) {
   let playerWeapon = getPlayerWeapon(event);
   let computerWeapon = getComputerWeapon();
   let roundWinner = getRoundWinner(playerWeapon, computerWeapon);
+
   updateScore(roundWinner);
+  viewPlayerSelection(event, roundWinner);
+  hideOtherSelections(event);
+  viewComputerSelection(computerWeapon);
+  viewRoundResult(roundWinner)
+  checkScore()
+  setTimeout(() => resetRound(), 3000)
 }
+
